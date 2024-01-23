f875812c40cea1dde634d012cc1fcb1a5f8ed664
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-23 18:53:42 +0530

    fonts added

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer, DefaultTheme} from "@react-navigation/native";
+import {useFonts} from "expo-font";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
@@ -16,14 +15,23 @@ const theme = {
 }
 
 const App = () => {
-  return (
-    <NavigationContainer theme={theme}>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"Home"}>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Details" component={Details} />
-        </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+    const [loaded] = useFonts({
+        InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+        InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+        InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+        InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+        InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    });
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={"Home"}>
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Details" component={Details}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
