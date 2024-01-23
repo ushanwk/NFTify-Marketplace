e0e7d58d8912f50d047756f2d2cf89be5005b65e
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-23 19:33:01 +0530

    nft card component images passes

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -9,6 +9,10 @@ const NFTCard = ({ data }) => {
 
     return(
         <View style={{ backgroundColor: COLORS.white, borderRadius: SIZES.font, marginBottom: SIZES.extraLarge, margin: SIZES.base, ...SHADOWS.dark }}>
+            <View style={{ width: "100%", height: 250 }}>
+                <Image source={data.image} resizeMode="cover" style={{ width: "100%", height: "100%",  borderTopLeftRadius: SIZES.font, borderTopRightRadius: SIZES.font }} />
+            </View>
+
             <Text>{data.name}</Text>
         </View>
     )
